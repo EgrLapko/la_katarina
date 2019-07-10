42c35595d5f865666cc86f5a570706ac5fc3b3f9
Yehor Lapko <yehorlapko@zoho.com>
2019-07-10 15:13:28 +0800

    about page remake has started

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,7 +7,17 @@ export default class About extends Component {
   render() {
     return (
       <div className="about-page">
-
+        <h2 className="title">About me</h2>
+        
+        <div className="info-box">
+          <div className="image-section">   
+            <img src="../images/katarina.jpg" alt=""/>
+          </div>
+          <div className="info-section">
+            <h2 className="about-name">My name is <span>Katarina</span></h2>
+          </div>
+        </div>
+        
       </div>
     )
   }
