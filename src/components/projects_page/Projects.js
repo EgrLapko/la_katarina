7bb5af27389db840d9f22abac6a4c97505e8b7e6
Yehor Lapko <yehorlapko@zoho.com>
2019-12-23 14:39:43 +0800

    images now live in internet

--- src/components/projects_page/Projects.js
+++ src/components/projects_page/Projects.js
@@ -18,15 +18,15 @@ export default class Projects extends Component {
                             <h3 className="projects-title">{cn ? chinese.projectsTitle : english.projectsTitle}</h3>
                             <div className="projects-container">
                                 <div className="project-item">
-                                    <Link to={`${match.url}/shamans`}><img src="../images/Projects/Covers/shamans_cover.jpg" alt=""/></Link>
+                                    <Link to={`${match.url}/shamans`}><img src="http://pic.yupoo.com/yehorlapko/3ba79344/482e2cef.jpg" alt=""/></Link>
                                     <Link to={`${match.url}/shamans`}><p className="project-title">{cn ? chinese.project1 : english.project1}</p></Link>
                                 </div>
                                 <div className="project-item">
-                                    <Link to={`${match.url}/ukrainian_beauty`}><img src="../images/Projects/Covers/uabeauty_cover.jpg" alt=""/></Link>
+                                    <Link to={`${match.url}/ukrainian_beauty`}><img src="http://pic.yupoo.com/yehorlapko/3975ff2c/26c3e11a.jpg" alt=""/></Link>
                                     <Link to={`${match.url}/ukrainian_beauty`}><p className="project-title">{cn ? chinese.project2 : english.project2}</p></Link>
                                 </div>
                                 <div className="project-item">
-                                    <Link to={`${match.url}/steampunk`}><img src="../images/Projects/Covers/steampunk_cover.jpg" alt=""/></Link>
+                                    <Link to={`${match.url}/steampunk`}><img src="http://pic.yupoo.com/yehorlapko/b7345f43/18453dec.jpg" alt=""/></Link>
                                     <Link to={`${match.url}/steampunk`}><p className="project-title">{cn ? chinese.project3 : english.project3}</p></Link>
                                 </div>
                             </div>  
--- src/components/projects_page/projects_images.js
+++ src/components/projects_page/projects_images.js
@@ -1,210 +1,210 @@
 const projects_images = [
     {
-        src: '/images/Projects/Pictures/shamans/1.jpg',
-        srcBig: '/images/Projects/Pictures/shamans/big/1.jpg',
+        src: 'http://pic.yupoo.com/yehorlapko/6adad43e/1c27c9ec.jpg',
+        srcBig: 'http://pic.yupoo.com/yehorlapko/7637002f/7e1779ad.jpg',
         id: 1,
         project: "shamans",
         position: "shamans column 1"
     },
     {
-        src: '/images/Projects/Pictures/shamans/2.jpg',
-        srcBig: '/images/Projects/Pictures/shamans/big/2.jpg',
+        src: 'http://pic.yupoo.com/yehorlapko/8624fafb/6cf8c5db.jpg',
+        srcBig: 'http://pic.yupoo.com/yehorlapko/808fbfdd/057b8b6c.jpg',
         id: 2,
         project: "shamans",
         position: "shamans column 1"
     },
     {
-        src: '/images/Projects/Pictures/shamans/3.jpg',
-        srcBig: '/images/Projects/Pictures/shamans/big/3.jpg',
+        src: 'http://pic.yupoo.com/yehorlapko/3d6f3cb2/0c9210d1.jpg',
+        srcBig: 'http://pic.yupoo.com/yehorlapko/e29387ef/7817a338.jpg',
         id: 3,
         project: "shamans",
         position: "shamans column 1"
     },
     {
-        src: '/images/Projects/Pictures/shamans/4.jpg',
-        srcBig: '/images/Projects/Pictures/shamans/big/4.jpg',
+        src: 'http://pic.yupoo.com/yehorlapko/105c62d9/dbdbc46b.jpg',
+        srcBig: 'http://pic.yupoo.com/yehorlapko/28603a82/01109fe6.jpg',
         id: 4,
         project: "shamans",
         position: "shamans column 1"
     },
     {
-        src: '/images/Projects/Pictures/shamans/5.jpg',
-        srcBig: '/images/Projects/Pictures/shamans/big/5.jpg',
+        src: 'http://pic.yupoo.com/yehorlapko/856c1d50/84f2aceb.jpg',
+        srcBig: 'http://pic.yupoo.com/yehorlapko/b5b6fd88/07c49bdc.jpg',
         id: 5,
         project: "shamans",
         position: "shamans column 1"
     },
     {
-        src: '/images/Projects/Pictures/shamans/6.jpg',
-        srcBig: '/images/Projects/Pictures/shamans/big/6.jpg',
+        src: 'http://pic.yupoo.com/yehorlapko/92a6df59/5a2828c8.jpg',
+        srcBig: 'http://pic.yupoo.com/yehorlapko/2a176ddb/cf5894fc.jpg',
         id: 6,
         project: "shamans",
         position: "shamans column 2"
     },
     {
-        src: '/images/Projects/Pictures/shamans/7.jpg',
-        srcBig: '/images/Projects/Pictures/shamans/big/7.jpg',
+        src: 'http://pic.yupoo.com/yehorlapko/4d526a8f/5a27fa4d.jpg',
+        srcBig: 'http://pic.yupoo.com/yehorlapko/e03eb8f8/bf8dbb65.jpg',
         id: 7,
         project: "shamans",
         position: "shamans column 2"
     },
     {
-        src: '/images/Projects/Pictures/shamans/8.jpg',
-        srcBig: '/images/Projects/Pictures/shamans/big/8.jpg',
+        src: 'http://pic.yupoo.com/yehorlapko/f4624258/f137eee8.jpg',
+        srcBig: 'http://pic.yupoo.com/yehorlapko/743313d0/2f501620.jpg',
         id: 8,
         project: "shamans",
         position: "shamans column 2"
     },
     {
-        src: '/images/Projects/Pictures/shamans/9.jpg',
-        srcBig: '/images/Projects/Pictures/shamans/big/9.jpg',
+        src: 'http://pic.yupoo.com/yehorlapko/17b3b62f/64a6f474.jpg',
+        srcBig: 'http://pic.yupoo.com/yehorlapko/76e9bbc3/374963ca.jpg',
         id: 9,
         project: "shamans",
         position: "shamans column 2"
     },
     {
-        src: '/images/Projects/Pictures/shamans/10.jpg',
-        srcBig: '/images/Projects/Pictures/shamans/big/10.jpg',
+        src: 'http://pic.yupoo.com/yehorlapko/a8fa3be7/16a6643a.jpg',
+        srcBig: 'http://pic.yupoo.com/yehorlapko/dd00cfbc/fc3c76a8.jpg',
         id: 10,
         project: "shamans",
         position: "shamans column 2"
     },
     {
-        src: '/images/Projects/Pictures/shamans/11.jpg',
-        srcBig: '/images/Projects/Pictures/shamans/big/11.jpg',
+        src: 'http://pic.yupoo.com/yehorlapko/692a029c/adf3429c.jpg',
+        srcBig: 'http://pic.yupoo.com/yehorlapko/71a12b5f/20884acc.jpg',
         id: 11,
         project: "shamans",
         position: "shamans column 3"
     },
     {
-        src: '/images/Projects/Pictures/shamans/12.jpg',
-        srcBig: '/images/Projects/Pictures/shamans/big/12.jpg',
+        src: 'http://pic.yupoo.com/yehorlapko/ed067172/6153db13.jpg',
+        srcBig: 'http://pic.yupoo.com/yehorlapko/773f3787/03236f40.jpg',
         id: 12,
         project: "shamans",
         position: "shamans column 3"
     },
     {
-        src: '/images/Projects/Pictures/shamans/13.jpg',
-        srcBig: '/images/Projects/Pictures/shamans/big/13.jpg',
+        src: 'http://pic.yupoo.com/yehorlapko/9b24e467/2251a8b9.jpg',
+        srcBig: 'http://pic.yupoo.com/yehorlapko/cac7bfd0/3d7acd36.jpg',
         id: 13,
         project: "shamans",
         position: "shamans column 3"
     },
     {
-        src: '/images/Projects/Pictures/shamans/14.jpg',
-        srcBig: '/images/Projects/Pictures/shamans/big/14.jpg',
+        src: 'http://pic.yupoo.com/yehorlapko/17a32533/0cd86fd9.jpg',
+        srcBig: 'http://pic.yupoo.com/yehorlapko/ac97c275/bd843fff.jpg',
         id: 14,
         project: "shamans",
         position: "shamans column 3"
     },
     {
-        src: '/images/Projects/Pictures/shamans/15.jpg',
-        srcBig: '/images/Projects/Pictures/shamans/big/15.jpg',
+        src: 'http://pic.yupoo.com/yehorlapko/2177a14b/b6de321c.jpg',
+        srcBig: 'http://pic.yupoo.com/yehorlapko/24b01bc2/54f75325.jpg',
         id: 15,
         project: "shamans",
         position: "shamans column 3"
     },
     {
-        src: '/images/Projects/Pictures/ukrainian/1.jpg',
-        srcBig: '/images/Projects/Pictures/ukrainian/big/1.jpg',
+        src: 'http://pic.yupoo.com/yehorlapko/0e24a1cf/29c6629b.jpg',
+        srcBig: 'http://pic.yupoo.com/yehorlapko/9d41e6c7/7a500fdd.jpg',
         id: 16,
         project: "ukrainian",
         position: "ukrainian column 1"
     },
     {
-        src: '/images/Projects/Pictures/ukrainian/2.jpg',
-        srcBig: '/images/Projects/Pictures/ukrainian/big/2.jpg',
+        src: 'http://pic.yupoo.com/yehorlapko/f4ad4063/5a816b68.jpg',
+        srcBig: 'http://pic.yupoo.com/yehorlapko/5b8bd343/1d06c956.jpg',
         id: 17,
         project: "ukrainian",
         position: "ukrainian column 1"
     },
     {
-        src: '/images/Projects/Pictures/ukrainian/3.jpg',
-        srcBig: '/images/Projects/Pictures/ukrainian/big/3.jpg',
+        src: 'http://pic.yupoo.com/yehorlapko/091f4d69/caa347c5.jpg',
+        srcBig: 'http://pic.yupoo.com/yehorlapko/c8c7b1e4/b7253979.jpg',
         id: 18,
         project: "ukrainian",
         position: "ukrainian column 1"
     },
     {
-        src: '/images/Projects/Pictures/ukrainian/4.jpg',
-        srcBig: '/images/Projects/Pictures/ukrainian/big/4.jpg',
+        src: 'http://pic.yupoo.com/yehorlapko/4dd31a71/4aa62733.jpg',
+        srcBig: 'http://pic.yupoo.com/yehorlapko/d4311ef7/e018d22c.jpg',
         id: 19,
         project: "ukrainian",
         position: "ukrainian column 2"
     },
     {
-        src: '/images/Projects/Pictures/ukrainian/5.jpg',
-        srcBig: '/images/Projects/Pictures/ukrainian/big/5.jpg',
+        src: 'http://pic.yupoo.com/yehorlapko/0309cc99/5561259f.jpg',
+        srcBig: 'http://pic.yupoo.com/yehorlapko/a997933e/72f5dc66.jpg',
         id: 20,
         project: "ukrainian",
         position: "ukrainian column 2"
     },
     {
-        src: '/images/Projects/Pictures/ukrainian/6.jpg',
-        srcBig: '/images/Projects/Pictures/ukrainian/big/6.jpg',
+        src: 'http://pic.yupoo.com/yehorlapko/3e899e36/dbf8cf95.jpg',
+        srcBig: 'http://pic.yupoo.com/yehorlapko/fd9806c0/bc7b4716.jpg',
         id: 21,
         project: "ukrainian",
         position: "ukrainian column 3"
     },
     {
-        src: '/images/Projects/Pictures/ukrainian/7.jpg',
-        srcBig: '/images/Projects/Pictures/ukrainian/big/7.jpg',
+        src: 'http://pic.yupoo.com/yehorlapko/539f54e9/accbb527.jpg',
+        srcBig: 'http://pic.yupoo.com/yehorlapko/28c0a1b1/7946a36a.jpg',
         id: 22,
         project: "ukrainian",
         position: "ukrainian column 3"
     },
     {
-        src: '/images/Projects/Pictures/steampunk/1.jpg',
-        srcBig: '/images/Projects/Pictures/steampunk/big/1.jpg',
+        src: 'http://pic.yupoo.com/yehorlapko/bdd0dcdd/290e465c.jpg',
+        srcBig: 'http://pic.yupoo.com/yehorlapko/db50b055/c44347ad.jpg',
         id: 23,
         project: "steampunk",
         position: "steampunk column 1"
     },
     {
-        src: '/images/Projects/Pictures/steampunk/2.jpg',
-        srcBig: '/images/Projects/Pictures/steampunk/big/2.jpg',
+        src: 'http://pic.yupoo.com/yehorlapko/8d4c92d3/3ed95f94.jpg',
+        srcBig: 'http://pic.yupoo.com/yehorlapko/31f3ee45/fbcdcd55.jpg',
         id: 24,
         project: "steampunk",
         position: "steampunk column 1"
     },
     {
-        src: '/images/Projects/Pictures/steampunk/3.jpg',
-        srcBig: '/images/Projects/Pictures/steampunk/big/3.jpg',
+        src: 'http://pic.yupoo.com/yehorlapko/9df58e4a/1211ae02.jpg',
+        srcBig: 'http://pic.yupoo.com/yehorlapko/34b6c611/2a0708af.jpg',
         id: 25,
         project: "steampunk",
         position: "steampunk column 1"
     },
     {
-        src: '/images/Projects/Pictures/steampunk/4.jpg',
-        srcBig: '/images/Projects/Pictures/steampunk/big/4.jpg',
+        src: 'http://pic.yupoo.com/yehorlapko/0924be3c/bedb523e.jpg',
+        srcBig: 'http://pic.yupoo.com/yehorlapko/7f61ba70/15534157.jpg',
         id: 26,
         project: "steampunk",
         position: "steampunk column 2"
     },
     {
-        src: '/images/Projects/Pictures/steampunk/5.jpg',
-        srcBig: '/images/Projects/Pictures/steampunk/big/5.jpg',
+        src: 'http://pic.yupoo.com/yehorlapko/f3b9877d/267928a4.jpg',
+        srcBig: 'http://pic.yupoo.com/yehorlapko/793e2d2c/4bbe6ea8.jpg',
         id: 27,
         project: "steampunk",
         position: "steampunk column 2"
     },
     {
-        src: '/images/Projects/Pictures/steampunk/6.jpg',
-        srcBig: '/images/Projects/Pictures/steampunk/big/6.jpg',
+        src: 'http://pic.yupoo.com/yehorlapko/e732f423/0b6171e9.jpg',
+        srcBig: 'http://pic.yupoo.com/yehorlapko/27b108b4/3c316648.jpg',
         id: 28,
         project: "steampunk",
         position: "steampunk column 2"
     },
     {
-        src: '/images/Projects/Pictures/steampunk/7.jpg',
-        srcBig: '/images/Projects/Pictures/steampunk/big/7.jpg',
+        src: 'http://pic.yupoo.com/yehorlapko/e7beb614/c0468572.jpg',
+        srcBig: 'http://pic.yupoo.com/yehorlapko/927da8f2/6ab53e47.jpg',
         id: 29,
         project: "steampunk",
         position: "steampunk column 3"
     },
     {
-        src: '/images/Projects/Pictures/steampunk/8.jpg',
-        srcBig: '/images/Projects/Pictures/steampunk/big/8.jpg',
+        src: 'http://pic.yupoo.com/yehorlapko/625009f3/bd762a9c.jpg',
+        srcBig: 'http://pic.yupoo.com/yehorlapko/31667e97/9d60e73b.jpg',
         id: 30,
         project: "steampunk",
         position: "steampunk column 3"
