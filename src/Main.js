486b4fce8dec3776cfd002c33a9022ca1e808e17
Yehor Lapko <yehorlapko@zoho.com>
2019-12-27 05:44:34 +0800

    deleted About and Contact components

--- src/Main.js
+++ src/Main.js
@@ -2,11 +2,9 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import MainPage from './components/mainPage/MainPage';
-import About from './components/About/About';
 import ImageGallery from './components/Gallery/ImageGallery';
 import Projects from './components/projects_page/Projects';
 import SingleProject from './components/projects_page/single_project/SingleProject';
-import Contact from './components/Contact/Contact';
 import Info from './components/Info/Info';
 
 export default class Main extends Component {
